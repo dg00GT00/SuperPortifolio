ef3f8d9152490d6ce5b83d5f461a22292206469f
Gledson Duarte <duarte.gledson@gmail.com>
2021-03-08 14:22:41 -0300

    Updated the file structure in order to work with webpack bundle

---
+++ src/script/formDynamicLabels.ts
@@ -0,0 +1,60 @@
+type IdsType = "username" | "email" | "message";
+type FormObjType = { [K in IdsType]: { labelEl?: HTMLLabelElement, inputValueLength: number } };
+
+const labelsEl = document.querySelectorAll("label");
+const formObj: FormObjType = {
+    email: {inputValueLength: 0},
+    message: {inputValueLength: 0},
+    username: {inputValueLength: 0}
+};
+
+labelsEl.forEach(label => {
+        const labelFor = label.getAttribute("for");
+        if (labelFor) {
+            formObj[labelFor as keyof FormObjType].labelEl = label;
+        }
+    }
+);
+
+const inputListener = (inputEl: HTMLInputElement | HTMLTextAreaElement): (ev: Event) => void => {
+    return ev => {
+        for (const obj in formObj) {
+            const typedObj = obj as keyof FormObjType;
+            const label = formObj[typedObj].labelEl;
+
+            if (label) {
+                if (obj === inputEl.getAttribute("id")) {
+                    formObj[typedObj].inputValueLength = inputEl.value.length;
+                    label.style.top = "-1.5em";
+                } else {
+                    if (formObj[typedObj].inputValueLength) {
+                        label.style.top = "-1.5em";
+                    } else {
+                        label.style.top = obj === "message" ? ".5em" : "calc(50% - 0.6rem)";
+                    }
+                }
+            }
+        }
+    };
+}
+
+for (const obj in formObj) {
+    const inputEl = document.getElementById(obj ?? "") as HTMLInputElement | HTMLTextAreaElement;
+    if (formObj.hasOwnProperty(obj)) {
+        if (inputEl) {
+            inputEl.oninput = inputEl.onfocus = inputListener(inputEl);
+            inputEl.onblur = ev => {
+                for (const obj in formObj) {
+                    const typedObj = obj as keyof FormObjType;
+                    const label = formObj[typedObj].labelEl;
+
+                    if (label) {
+                        if (!inputEl.value.length && obj === inputEl.getAttribute("id")) {
+                            label.style.top = obj === "message" ? ".5em" : "calc(50% - 0.6rem)";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ webpack.config.ts
@@ -0,0 +1,70 @@
+import path from "path";
+import webpack from "webpack";
+// @ts-ignore
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: webpack.Configuration = {
+    context: __dirname, // to automatically find tsconfig.json
+    mode: "production",
+    entry: {main: "./src/script/main.ts"},
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+                options: {
+                    // disable type checker - we will use it in fork plugin
+                    transpileOnly: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    "resolve-url-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                outputStyle: "compressed"
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|webp)$/i,
+                // @ts-ignore
+                type: "asset/resource"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".scss"]
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        })
+    ]
+};
+
+export default config;
